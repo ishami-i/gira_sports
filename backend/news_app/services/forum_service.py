56638f052293cab39848f80e6a6314e54a7c3b50
ishami-i <i.irene@alustudent.com>
2026-05-30 10:45:18 +0200

    added forum service

--- backend/news_app/services/forum_service.py
+++ backend/news_app/services/forum_service.py
@@ -1,28 +1,23 @@
 from datetime import datetime
 from django.utils import timezone
-
-# getting serielized data of the forum model
-from news_app.serializers import ForumSerializer
+from ..models.forum_post import ForumPost
 
 # getting all the forum posts
 def get_all_forum_posts():
     """
     Get all forum posts.
     """
-    from news_app.models.forum import ForumPost
-    forum_posts = ForumPost.objects.all().order_by("-created_at")
-    return ForumSerializer(forum_posts, many=True).data
+    return ForumPost.objects.all().order_by("-created_at")
 
 # adding a new forum post to the database
 def add_forum_post(title, content, author):
     """
     Add a new forum post to the database.
     """
-    from news_app.models.forum import ForumPost
     forum_post = ForumPost.objects.create(
         title=title,
         content=content,
         author=author,
         created_at=timezone.now()
     )
-    return ForumSerializer(forum_post).data
+    return forum_post
