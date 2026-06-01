1b254396647c5d74b6ae49639fd3a6a6a3b372d7
ishami-i <i.irene@alustudent.com>
2026-06-01 23:03:15 +0200

    created a view for delete, update, and views incrementing

--- backend/news_app/views/article_views.py
+++ backend/news_app/views/article_views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from ..services.article_service import get_trending_articles, get_all_articles
+from ..services.article_service import get_trending_articles, get_all_articles, add_article, update_article, delete_article, increment_article_views
 # from rest_framework import generics
 
 # Create your views here.
@@ -57,5 +57,41 @@ def add_article(request):
             'category': article.category.name
         }
         return JsonResponse(data)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
+    
+
+# updating an existing article in the database
+def update_article(request, article_id, author_id):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        author = request.POST.get('author_id')
+        article = update_article(article_id, title, content, author)
+        if article:
+            data = {
+                'title': article.title,
+                'slug': article.slug,
+                'content': article.content,
+                'published_at': article.published_at,
+                'views': article.views,
+                'status': article.status,
+                'author': article.author.name,
+                'category': article.category.name
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({'error': 'Article not found'}, status=404)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
+    
+# deleting an article from the database
+def delete_article(request, article_id):
+    if request.method == 'DELETE':
+        success = delete_article(article_id)
+        if success:
+            return JsonResponse({'message': 'Article deleted successfully'})
+        else:
+            return JsonResponse({'error': 'Article not found'}, status=404)
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=400)
